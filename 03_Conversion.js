9c13dee72822c0269de00a8cdeb7163028465199
abhishekkumarjha <abhishekkjha068@outlook.com>
2023-09-05 01:41:15 +0530

    conversion

---
+++ 03_Conversion.js
@@ -0,0 +1,25 @@
+let score = undefined;
+
+console.log(typeof score);
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber)
+console.log(valueNumber)
+
+//"33" => 33
+//"33abc"=> NaN
+// true =>1; false =>0;
+
+
+let isLoggedIn = ""
+
+let booleanIsloggedIn = Boolean(isLoggedIn);
+console.log(booleanIsloggedIn)
+
+//1 => true , 0 ==>false
+//"hitesh" ==>true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
