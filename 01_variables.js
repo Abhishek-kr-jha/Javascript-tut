91f7efa706a71377892d58649f40fa934b9d8840
abhishekkumarjha <abhishekkjha068@outlook.com>
2023-09-05 01:25:52 +0530

    javascript-Datatypes

---
+++ 01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 145523
+let accountEmail = "abhishek@gmail.com"
+var accountPassword="12344"
+accountCity = "Jaipur"
+let accountState;
+
+
+/*
+Prefer not to use var 
+because issue in block scope and functional scope
+
+*/
+// accountId = 2
+accountEmail = "Hc@hc.com";
+accountPassword ="2121212";
+accountCity="Banagalore";
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ 02_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code as newer version of code
+
+// alert("Hello......!!") // we  are using nodejs , not browser
+
+
+let name = "Abhi";
+let age = 18;
+let isloggedIn =  false
+
+
+//number
+//bigInt
+// string =>""
+// boolean => true/false
+// null => standalone value
+//undefined =>
+// symbol=> unique
+
+
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)// objects
+
+
